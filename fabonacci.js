01926112c6b1e427fc6a49028799c7daeeb0318d
Rebecca <rebecca.yu@senao.com.tw>
2023-02-06 10:29:55 +0800

    增加註解

--- fabonacci.js
+++ fabonacci.js
@@ -7,3 +7,7 @@ function factorial(num) {
 }
 
 console.log(factorial(6))
+
+// 之所以能夠透過遞迴函式，是因為函式堆疊（stack）在執行時有一個特性，
+// 當某個函式呼叫另一個函式時，需要等到裡面的函式執行完產生結果後，才會繼續回來執行自己的函式內容，
+// 而這樣的情況也被稱作 後進先出（Last in First Out, LIFO）
