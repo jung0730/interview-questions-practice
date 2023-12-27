fe3d4182cfadc33a386f75769acd3643ac325ff1
jung0730 <arj2763@gmail.com>
2023-12-27 18:24:25 +0800

    remove original lodashGet logic

--- lodash-get.js
+++ lodash-get.js
@@ -1,18 +1,8 @@
 function lodashGet(object, path, defaultValue) {
-  if (object === null) {
-    return defaultValue
-  }
-  let count = 0;
-  const length = path.length;
-  while (object !== undefined && count < length) {
-    object = object[path[count++]];
-  }
-  const result = count === length ? object : undefined;
-  return result === undefined ? defaultValue : result;
 }
 
 const object = { a: [{ b: { c: 3 } }] };
 console.log(lodashGet(object, ['a', '0', 'b', 'c'], 'default'));
-console.log(lodashGet(object, ['a', '1', 'b', 'c'], 'default'));
+console.log(lodashGet({ a: { b: 2 }, c: 1 }, 'a.b'));
 
 // Writing object != null would check for undefined and null at the same time.
