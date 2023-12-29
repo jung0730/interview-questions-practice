187c5afceeb82377a60ecdca8951aedf306e5e55
jung0730 <arj2763@gmail.com>
2023-12-29 11:33:41 +0800

    selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,21 @@
+// works great in small array
+// time complexity of O(n2)
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // Initialize index of min element to the start of the unsorted section.
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found min element with the first element of the unsorted section.
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([9, 3, 6, 2, 4, 11]))
